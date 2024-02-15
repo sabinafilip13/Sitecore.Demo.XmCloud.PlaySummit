ed59e149c7b6c74b5734429d98972c6b04402253
Sabina Filip <sabina.filip@sagittarius.agency>
2024-02-15 16:59:04 +0000

    Try adding moosend embed script to component

--- src/rendering/src/components/Forms/EmbedSendForm.tsx
+++ src/rendering/src/components/Forms/EmbedSendForm.tsx
@@ -12,6 +12,7 @@ type EmbedSendFormProps = ComponentProps & {
 const EmbedSendForm = (props: EmbedSendFormProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
   const sxaStyles = `${props.params?.styles || ''}`;
+  const formId = props?.fields?.sendFormId.replace(/-/g, "");
 
   if (isEditingOrPreviewingPage(sitecoreContext.pageState)) {
     const isEditing = sitecoreContext.pageEditing;
@@ -25,6 +26,7 @@ const EmbedSendForm = (props: EmbedSendFormProps): JSX.Element => {
     );
 
     return (
+      <script>if(!window.mootrack){ !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","https://cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack"); } mootrack('loadForm', formId);</script>
       <section className={`section embed-send-form ${sxaStyles}`}>
         <div className="section-content col-content container">
           <p className="component-title">Sitecore Send Form Component</p>
