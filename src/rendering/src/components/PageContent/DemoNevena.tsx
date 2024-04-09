59a0cfaf754814c81523c57f8f9a71a3ac34c022
sabinafilip13 <sabina.filip@sagittarius.agency>
2024-04-09 12:50:35 +0100

    Fix more build errors

--- src/rendering/src/components/PageContent/DemoNevena.tsx
+++ src/rendering/src/components/PageContent/DemoNevena.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Field, Text, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 
-
 type DemoNevenaProps = ComponentProps & {
   fields: {
     title: Field<string>;
@@ -11,19 +10,16 @@ type DemoNevenaProps = ComponentProps & {
 };
 
 const HeroImage = (props: DemoNevenaProps): JSX.Element => {
-  const id = props.params.RenderingIdentifier; 
-    return (
-      <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
+  const id = props.params.RenderingIdentifier;
+  return (
+    <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
       <div className="component-content">
         <p>DemoNevena Component</p>
         <Text tag="h2" field={props.fields?.title} />
         <Text tag="h5" field={props.fields?.description} />
       </div>
     </div>
-    );
-  
-
+  );
 };
 
 export const Default = withDatasourceCheck()<DemoNevenaProps>(HeroImage);
-
